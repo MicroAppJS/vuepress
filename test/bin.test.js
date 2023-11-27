f3f7b3ee96e03dc76b622769328e64f15ec92a42
Zyao89 <zyao89@gmail.com>
2023-11-27 20:05:13 +0800

    feat: update

--- test/bin.test.js
+++ test/bin.test.js
@@ -5,14 +5,24 @@ process.env.MICRO_APP_TEST = 'true';
 
 describe('VuePress Bin test', () => {
 
-    it('dev run docs', () => {
+    it('help run', async () => {
 
         try {
-            require('../bin/micro-app-vuepress');
+            const { service } = require('@micro-app/cli');
+            await service.run('vuepress');
         } catch (error) {
             console.warn(error);
         }
+    });
 
+    it('dev run docs', async () => {
+
+        try {
+            const { service } = require('@micro-app/cli');
+            await service.run('vuepress', { _: [ 'dev', 'docs' ] });
+        } catch (error) {
+            console.warn(error);
+        }
     });
 
 });
